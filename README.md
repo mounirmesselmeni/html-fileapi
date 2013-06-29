4a620505120f95ff2b1844a0bd40229e6d269bf4
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2013-06-29 19:46:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Reading CSV file with Javascript and HTML5 File API
+===============
+
+Run
+---
+
+Just clone the project and run `index.html`.
+
+File API
+--------
+
+HTML5 finally provides a standard way to interact with local files, via the [File API][1]
+
+
+FileReader includes four options for reading a file, asynchronously:
+
+- `FileReader.readAsBinaryString(Blob|File)` - The result property will contain the file/blob's data as a binary string. Every byte is represented by an integer in the range [0..255].
+- `FileReader.readAsText(Blob|File, opt_encoding)` - The result property will contain the file/blob's data as a text string. By default the string is decoded as 'UTF-8'. Use the optional encoding parameter can specify a different format.
+- `FileReader.readAsDataURL(Blob|File)` - The result property will contain the file/blob's data encoded as a data URL.
+- `FileReader.readAsArrayBuffer(Blob|File)` - The result property will contain the file/blob's data as an ArrayBuffer object.
+
+For our example, we want to read a CSV file, so we can read it as text. And perfom data processing after in `processData(csv)` function.
+
+
+You can read this [post][2] for more details.
+
+
+[1]: http://www.w3.org/TR/file-upload/
+[2]: http://mounirmesselmeni.github.io/
---
+++ js/read-csv.js
@@ -0,0 +1,58 @@
+function handleFiles(files) {
+	// Check for the various File API support.
+	if (window.FileReader) {
+		// FileReader are supported.
+		getAsText(files[0]);
+	} else {
+		alert('FileReader are not supported in this browser.');
+	}
+}
+
+function getAsText(fileToRead) {
+	var reader = new FileReader();
+	// Read file into memory as UTF-8      
+	reader.readAsText(fileToRead);
+	// Handle errors load
+	reader.onload = loadHandler;
+	reader.onerror = errorHandler;
+}
+
+function loadHandler(event) {
+	var csv = event.target.result;
+	processData(csv);             
+}
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+    for (var i=0; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(';');
+            var tarr = [];
+            for (var j=0; j<data.length; j++) {
+                tarr.push(data[j]);
+            }
+            lines.push(tarr);
+    }
+	console.log(lines);
+	drawOutput(lines);
+}
+
+function errorHandler(evt) {
+	if(evt.target.error.name == "NotReadableError") {
+		alert("Canno't read file !");
+	}
+}
+
+function drawOutput(lines){
+	//Clear previous data
+	document.getElementById("output").innerHTML = "";
+	var table = document.createElement("table");
+	for (var i = 0; i < lines.length; i++) {
+		var row = table.insertRow(-1);
+		for (var j = 0; j < lines[i].length; j++) {
+			var firstNameCell = row.insertCell(-1);
+			firstNameCell.appendChild(document.createTextNode(lines[i][j]));
+		}
+	}
+	document.getElementById("output").appendChild(table);
+}
