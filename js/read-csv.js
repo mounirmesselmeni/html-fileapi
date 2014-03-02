e896197ce62cee0d170460bab9ee754c4b626198
skbohrer <skbohrer@gmail.com>
2014-03-02 02:27:43 -0500

    Update read-csv.js
    typo in line 29: split on , not ;

--- js/read-csv.js
+++ js/read-csv.js
@@ -26,7 +26,7 @@ function processData(csv) {
     var allTextLines = csv.split(/\r\n|\n/);
     var lines = [];
     for (var i=0; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(';');
+        var data = allTextLines[i].split(',');
             var tarr = [];
             for (var j=0; j<data.length; j++) {
                 tarr.push(data[j]);
